aef01be6a7275140686f4dc932176147bc6af114
Stuart Murray <stuart.murray.dev@outlook.com>
2026-07-09 10:12:27 +0100

    End of section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
